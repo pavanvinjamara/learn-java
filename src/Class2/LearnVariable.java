e871893029c100c734a3dfdc80ce2739fa38387e
Vinjamara Pavan Kumar <109328446+pavanvinjamara@users.noreply.github.com>
2025-04-11 18:14:44 +0530

    started class 2

--- src/Class2/LearnVariable.java
+++ src/Class2/LearnVariable.java
@@ -2,6 +2,6 @@ package src.Class2;
 
 public class LearnVariable {
     public static void main(String[] args){
-        System.out.println("Started Variable");
+        System.out.println("Started Variables");
     }
 }
