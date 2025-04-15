45e787b247489ecba32824e82f8421ead9ac7e45
pavanvinjamara <pavanvinjamara323@gmail.com>
2025-04-16 00:28:13 +0530

    learn variable

--- src/Class2/LearnVariable.java
+++ src/Class2/LearnVariable.java
@@ -2,6 +2,22 @@ package src.Class2;
 
 public class LearnVariable {
     public static void main(String[] args){
-        System.out.println("Started Variables");
+        // To show Output
+        System.out.print("Started Variables");
+        //   To Print in new line we use
+        System.out.println("new line");
+
+        // Variable
+        int a = 1;
+        String name = "pavan kumar";
+        double price = 20.25;
+
+        // Data Type
+        // Two data types - 1)Primitive, 2) Non-primitive
+
+        // Primitive Data Type
+        // Basic Data type which are already present in java
+
+
     }
 }
