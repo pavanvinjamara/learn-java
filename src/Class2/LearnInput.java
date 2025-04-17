15876b25d4bd02ce440065a0b958a4669c9734ee
pavanvinjamara <pavanvinjamara323@gmail.com>
2025-04-18 00:00:27 +0530

    input from terminal Scanner  class

---
+++ src/Class2/LearnInput.java
@@ -0,0 +1,56 @@
+package src.Class2;
+import java.util.Scanner;// which imports all utils;
+
+public class LearnInput {
+    public static void main(String[] args){
+        //   Take Input from terminal
+        //   Using Scanner class we made an object using new keyword and class
+        // which take input using passing (System.in) in scanner Class
+        // To use Scanner Class we should import java utils using import key word
+        Scanner sc = new Scanner(System.in);
+//        Scanner:
+//        A class in Java that is used to read input from various sources, like the keyboard, a file, etc.
+//
+//        new Scanner(System.in):
+//        Creates a new Scanner object that will read input from the keyboard (console).
+//
+//                System.in represents standard input (i.e., the keyboard).
+//
+//                new Scanner(...) creates the object.
+//
+//                sc:
+//        The name of the Scanner object. You can use any name, but sc is short for “scanner”.
+        // "Create a new Scanner object that reads from the keyboard and name it sc."
+        String name = sc.next();
+        System.out.println(name);
+
+//        sc.next():
+//        This reads the next word (token) typed by the user. It stops reading when the user hits the space or Enter key.
+//
+//        Example:
+//
+//        User types: John Smith
+//
+//        sc.next() reads only: John (it stops at the space)
+//
+//        String name:
+//        Declares a variable named name of type String to store the input.
+//
+//🔄 Full meaning:
+//        "Read the next word typed by the user from the console and store it in a string variable named name."
+    // To Read Whole Line
+    // we use sc.nextLine()
+        // for String next() and nextLine();
+        // for int nextInt();
+    String user = sc.nextLine();
+    System.out.println(user);
+//    int age = sc.nextInt();
+//        double height = sc.nextDouble();
+//
+//        // Read boolean (true/false)
+//        System.out.print("Are you a student? (true/false): ");
+//        boolean isStudent = sc.nextBoolean();
+//        System.out.println(user);
+
+    }
+}
--- src/Class2/LearnVariable.java
+++ src/Class2/LearnVariable.java
@@ -16,7 +16,31 @@ public class LearnVariable {
         // Two data types - 1)Primitive, 2) Non-primitive
 
         // Primitive Data Type
-        // Basic Data type which are already present in java
+            // Basic Data type which are already present in java
+            //   byte, short,char,boolean, int, long,float,double
+            //   Memory Space
+                // byte - 1 bytes (1 bit = 01) 1byte = 8bits(01010101)
+                // char - 2 bytes
+                // boolean - 1 bytes
+                // int - 4 bytes
+                // long - 8 bytes
+                // float - 4 bytes
+                // double - 8 bytes
+                // In depends on system above bytes are according to 64 bit ,
+                // if it is  32-bit int is 2 byte only.
+        //  Non-Primitive Data Type
+            //   which we can build them
+            // String, Array, Class, Object, Interface
+
+        // Do some mathematical problems
+        int b = 25;
+        int c = 10;
+
+        int ans1 = b*c/b-c;
+        System.out.println(ans1);
+        int ans2 = (b*c)/(b-c);
+        System.out.println(ans2);
+
 
 
     }
