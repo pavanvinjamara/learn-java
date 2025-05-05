2d7633a3e41643a29bcdb4185dac14a6b0704c38
pavanvinjamara <pavanvinjamara323@gmail.com>
2025-05-05 23:18:26 +0530

    intro binearch search

---
+++ src/binarySearch/BinarySearch.java
@@ -0,0 +1,12 @@
+package src.binarySearch;
+
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target){
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
