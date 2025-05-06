a650f8968ac888c5f75cd5594fdcfcfc2d05dfb9
pavanvinjamara <pavanvinjamara323@gmail.com>
2025-05-06 23:47:55 +0530

    intro binearch search

--- src/binarySearch/BinarySearch.java
+++ src/binarySearch/BinarySearch.java
@@ -2,7 +2,14 @@ package src.binarySearch;
 
 public class BinarySearch {
     public static int binarySearch(int[] arr, int target){
+        //Starting index
+        int low = 0;
+        // Ending Index
+        int high = arr.length-1;
 
+        while (low <= high){
+            int mid = low + (high-low)/2;
+        }
         return -1;
     }
 
