18a24485b77dccb5d7b6814ba38bfa303dad1709
pavanvinjamara <pavanvinjamara323@gmail.com>
2025-06-10 23:09:02 +0530

    learn package and class

---
+++ src/learnPackageAndClass/Dog.java
@@ -0,0 +1,13 @@
+package src.learnPackageAndClass;
+
+public class Dog {
+    public String Breed = "German shepard";
+    public int height = 20;
+
+
+    public static void main(String[] args) {
+            Dog dog = new Dog();
+
+            System.out.println(dog.Breed);
+    }
+}
