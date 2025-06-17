83b8df03f68e05b49cf1f5386508d685a108bf63
pavanvinjamara <pavanvinjamara323@gmail.com>
2025-06-17 09:35:03 +0530

    learn started Methods

---
+++ src/learnMethods/Bank.java
@@ -0,0 +1,16 @@
+package src.learnMethods;
+
+// To Understand methods we are using bank application
+public class Bank {
+    static int currentBalance = 1000;
+
+    // Creating greeting methods, because when user opens the application he/she saw greetings
+    public static void greetingCustomer(){
+        System.out.println("Hello Pavan, welcome to hello world banking app");
+    }
+
+    public static void main(String[] args) {
+        // call the method to run the method
+        greetingCustomer();
+    }
+}
