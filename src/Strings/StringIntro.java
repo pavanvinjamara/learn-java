78a3923dc806effa5c31b67d1ac8c8c131b1bcee
vinjamara pavan kumar <pavanvinjamara323@gmail.com>
2025-07-28 18:57:00 +0530

    started string

---
+++ src/Strings/StringIntro.java
@@ -0,0 +1,8 @@
+package src.Strings;
+
+public class StringIntro {
+    public static void main(String[] args) {
+        //  StringClass variableName = " value ";
+            String name = "Vinjamara Pavan kumar";
+    }
+}
