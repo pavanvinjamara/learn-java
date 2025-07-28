c3018a734e1ce953256259cc48f75f35ad8d9b95
vinjamara pavan kumar <pavanvinjamara323@gmail.com>
2025-07-28 17:14:51 +0530

    completed foreach loop

--- src/loopingStatements/ForeachLoop.java
+++ src/loopingStatements/ForeachLoop.java
@@ -3,5 +3,32 @@ package src.loopingStatements;
 public class ForeachLoop {
     public static void main(String[] args) {
         // Foreach loop is extension method of for loop
+        // foreach can have limitation, we can use only for collection and Array
+        // for and foreach only differ from syntax.
+        // ** Syntax :-
+        //      => for(dataType variableName: arrayOrCollection){
+        //          //statements
+        //      }
+        // foreach cannot suit for decremetion, cannot access last to first
+        // we cannot get the index in foreach loop
+
+        int[] arr = {1,2,3,4,5,6,7};
+        for( int item:arr){
+            System.out.println(item);
+        }
+
+        int[][] arr2 = {
+                {1, 2 ,3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for(int[] singleArr:arr2){
+            for(int value:singleArr){
+                System.out.println(value+"---");
+            }
+        }
+
+        // same goes with jagged array
     }
 }
