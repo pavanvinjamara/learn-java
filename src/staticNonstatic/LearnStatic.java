e0fb807e4773fc219a406a663bc93c38dc5465a0
vinjamara pavan kumar <pavanvinjamara323@gmail.com>
2026-01-28 16:07:27 +0530

    completed static and non-static

--- src/staticNonstatic/LearnStatic.java
+++ src/staticNonstatic/LearnStatic.java
@@ -3,17 +3,20 @@ package src.staticNonstatic;
 import java.util.Arrays;
 
 public class LearnStatic {
-    static int number;
+    static int number = 12;
     public static void main(String[] args){
         System.out.println("main method");
 //        Access the static variable and assign the value
         number = 10;
+        LearnStatic.number = 12;
         System.out.println(number);
 //        accessing static variable from another class
         System.out.println(Test1.number2);
 
-    }
+        new LearnStatic(); // By running this we get to know non-static block created when constructor called.
 
+    }
+// Blocks are used for variable Initialization
 //    creating static block
     static {
         System.out.println("static block");
@@ -24,9 +27,14 @@ public class LearnStatic {
     }
 
 //    constructor
+//    By default constructor creates a non-static block
     public LearnStatic(){
 //        There is no static constructor we have only non-static constructor
         System.out.println("constructor");
     }
 
+
+//    Note: before executing constructor it executes user defined non-static block is created so used that if any initialization we can do before executing constructor.
+//    Note : static block execute in defined order and it executes before main method.
+    // we can call static methods in non-static, but we cannot non-static methods in static method.
 }
