90756e3c3c6bc3e7d4605d3ae638a40c69b560e7
vinjamara pavan kumar <pavanvinjamara323@gmail.com>
2026-02-23 19:33:06 +0530

    Compeleting handling property file

---
+++ src/handlePropertyFile/LearnPropertyFile.java
@@ -0,0 +1,60 @@
+package src.handlePropertyFile;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class LearnPropertyFile {
+    public static void main(String[] args) throws IOException {
+        File file = new File("./src/handlePropertyFile/dbConfig.properties");
+
+        if(!file.exists()){
+            file.createNewFile();
+            System.out.println("absent");
+        }else{
+            System.out.println("Present");
+        }
+
+//      To create properties in property file
+        Properties prop = new Properties();
+//        creating properties
+        prop.setProperty("url", "localhost");
+        prop.setProperty("username", "pavan@323");
+        prop.setProperty("password", "hyyr123");
+
+//        To Add properties in file
+        FileOutputStream fos = new FileOutputStream(file);
+        prop.store(fos, "New File is created");
+
+//        To Read file
+        FileInputStream fis = new FileInputStream(file);
+        prop.load(fis);
+
+//        To load keys
+        Set<String> keys = prop.stringPropertyNames();
+        System.out.println(keys);
+//        for we can use keySet() || prop.keys
+
+//        For values
+
+        Collection<Object> values = prop.values();
+        System.out.println(values);
+
+//        For key, we need to get value
+//        If we don't have key we give default value
+            System.out.println(prop.getProperty("time", "default time"));
+
+            System.out.println(prop);
+//            we can also iterate using foreach loop
+            for(Object key: prop.keySet()){
+                System.out.println(key+ " > "+ prop.getProperty((String) key));
+            }
+            Set<Map.Entry<Object, Object>> data = prop.entrySet();
+            for(Map.Entry<Object, Object> entry:data){
+//                System.out.println(entry.getKey() +" > "+ getV );
+            }
+
+    }
+}
